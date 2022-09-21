2814521b721854a11dd04fc960168632797a1ece
unrooot <ty@n4w.to>
2022-09-20 22:08:53 -0700

    fix: increase max depth size

--- src/DrawVisualizer/modules/Shared/VisualizerConstants.lua
+++ src/DrawVisualizer/modules/Shared/VisualizerConstants.lua
@@ -1,8 +1,10 @@
 return {
-	PLUGIN_ICON = "",
-	PLUGIN_NAME = "Draw Visualizer",
-	PLUGIN_DESC = "temporary description lol",
-	ACTION_NAME = "Toggle Draw Visualizer",
-	ACTION_DESC = "Toggles the Draw Visualizer debug UI.",
-	TOOLBAR_LABEL = "unroot's plugins"
+	PLUGIN_ICON = "";
+	PLUGIN_NAME = "Draw Visualizer";
+	PLUGIN_DESC = "temporary description lol";
+	ACTION_NAME = "Toggle Draw Visualizer";
+	ACTION_DESC = "Toggles the Draw Visualizer debug UI.";
+	TOOLBAR_LABEL = "unroot's plugins";
+
+	MAX_DEPTH_SIZE = 500;
 }
